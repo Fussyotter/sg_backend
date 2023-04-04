5a3d5853acba7f5c8c39e890e0827faceb1d3e29
ialoysious <isaacaloysious@gmail.com>
2023-04-04 09:51:17 -0700

    asgi test

--- ssage/ssage/settings.py
+++ ssage/ssage/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'daphne',
+    'daphne',
     'channels',
     # 'django.contrib.messages',
     'djoser',
@@ -89,6 +89,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ssage.wsgi.application'
+ASGI_APPLICATION = "ssage.asgi.application"
+
 
 
 # Database
