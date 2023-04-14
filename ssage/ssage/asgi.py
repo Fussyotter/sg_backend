43f662e94bd6e3f35608639bfd4171bcc2d389cc
ialoysious <isaacaloysious@gmail.com>
2023-04-14 14:19:37 -0700

    what fun

--- ssage/ssage/asgi.py
+++ ssage/ssage/asgi.py
@@ -24,28 +24,35 @@
 
 # # Wrap the application with the channel layer
 # application = channel_layer.asgi()(application)
+# import os
+# from django.core.asgi import get_asgi_application
+# from django.urls import re_path
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from channels.security.websocket import AllowedHostsOriginValidator
+# import ssage_api.routing
+
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+# # Initialize Django ASGI application early to ensure the AppRegistry
+# # is populated before importing code that may import ORM models.
+# django_asgi_app = get_asgi_application()
+
+# application = ProtocolTypeRouter(
+#     {
+#     "http": django_asgi_app,
+#     # Just HTTP for now. (We can add other protocols later.)
+#     "websocket": AllowedHostsOriginValidator(
+#     AuthMiddlewareStack(
+#         URLRouter(
+#             ssage_api.routing.websocket_urlpatterns
+#         ))
+#     )
+# })
 import os
+
 from django.core.asgi import get_asgi_application
-from django.urls import re_path
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-import ssage_api.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_main.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter(
-    {
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
-    "websocket": AllowedHostsOriginValidator(
-    AuthMiddlewareStack(
-        URLRouter(
-            ssage_api.routing.websocket_urlpatterns
-        ))
-    )
-})
+application = get_asgi_application()
